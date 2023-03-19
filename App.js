cb313f2b18d59314d755fc391c3a6ab983f318fb
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2023-03-19 23:27:57 +0200

    button added with functionality(addNumber, decreaseNumber)

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function buttonPressIncrease() {
+    setCount((prev) => prev + 1);
+  }
+  function buttonPressDecrease() {
+    setCount((prev) => prev - 1);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Button
+          style={styles.MainButton}
+          title="Increase number"
+          onPress={buttonPressIncrease}
+        />
+        <Text id="number">{count}</Text>
+        <StatusBar style="auto" />
+        <Button
+          style={styles.MainButton}
+          title="Decrease number"
+          onPress={buttonPressDecrease}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -16,5 +39,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  MainButton: {
+    borderRadius: 10,
+    backgroundColor: "grey",
+    width: 60,
+    color: "grey",
   },
 });
